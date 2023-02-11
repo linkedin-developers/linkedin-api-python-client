77cd68e6275975d865b5fe3cfe61243c597caac9
Steven Shimizu <sshimizu@linkedin.com>
2023-02-10 23:24:44 -0800

    fix: fixed examples (#2)

--- examples/crud_ad_accounts.py
+++ examples/crud_ad_accounts.py
@@ -1,26 +1,34 @@
+"""
+Example calls to perform CRUD and finder operations on ad accounts using versioned APIs.
+This will create a test ad account, fetch it directly, find it using search criteria, update it, and delete it.
 
-# Sets the path to locate local linkedin_api_client module
-from dotenv import load_dotenv, find_dotenv
-from linkedin_api.restli_client.client import RestliClient
-from linkedin_api.restli_client.response import CollectionResponse
-import json
-import os
-import sys
+The 3-legged member access token should include the 'rw_ads' scope, which is part of the
+Advertising APIs product.
+"""
+
+import os,sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-# Load the app credentials info from the examples/.env file
+from dotenv import load_dotenv,find_dotenv
 load_dotenv(find_dotenv())
-from typing import Type
+
+from linkedin_api.clients.restli.client import RestliClient
+import json
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 if ACCESS_TOKEN is None:
   raise Exception('A valid access token must be defined in the /examples/.env file under the variable name "ACCESS_TOKEN"')
 
 AD_ACCOUNTS_RESOURCE = '/adAccounts'
-MDP_VERSION = '202212'
+AD_ACCOUNTS_ENTITY_RESOURCE = '/adAccounts/{id}'
+API_VERSION = '202212'
 
 restli_client = RestliClient()
+restli_client.session.hooks['response'].append(lambda r: r.raise_for_status())
 
+
+"""
+Create a test ad account
+"""
 create_response = restli_client.create(
     resource_path=AD_ACCOUNTS_RESOURCE,
     entity={
@@ -31,43 +39,78 @@ create_response = restli_client.create(
       "test": True
     },
     access_token=ACCESS_TOKEN,
-    version_string=MDP_VERSION
+    version_string=API_VERSION
   )
-print(f"Successfully created ad account: {create_response.entity_id}")
+ad_account_id = create_response.entity_id
+print(f"Successfully created ad account: {ad_account_id}\n")
 
-restli_client.delete(
-  resource_path=f"{AD_ACCOUNTS_RESOURCE}/{id}",
+
+"""
+Get the created ad account
+"""
+get_response = restli_client.get(
+  resource_path=AD_ACCOUNTS_ENTITY_RESOURCE,
   path_keys={
-    "id": create_response.entity_id
+    "id": ad_account_id
   },
   access_token=ACCESS_TOKEN,
-  version_string=MDP_VERSION
+  version_string=API_VERSION
 )
-print(f"Successfully deleted ad account.")
+print(f"Successfully fetched ad account: {json.dumps(get_response.entity)}\n")
 
 
-'''
+"""
+Partial update on ad account
+"""
+partial_update_response = restli_client.partial_update(
+  resource_path=AD_ACCOUNTS_ENTITY_RESOURCE,
+  path_keys={
+    "id": ad_account_id
+  },
+  patch_set_object={
+    "name": "Modified Test Ad Account"
+  },
+  access_token=ACCESS_TOKEN,
+  version_string=API_VERSION
+)
+print(f"Successfully updated ad account\n")
+
+
+"""
 Find ad accounts by search criteria
-'''
+"""
 r = restli_client.finder(
     resource_path=AD_ACCOUNTS_RESOURCE,
-    finder_name='search',
+    finder_name="search",
     query_params={
         "search": {
-            "status": {
-                "values": ["ACTIVE", "DRAFT", "CANCELED"]
+            "reference": {
+              "values": ["urn:li:organization:123"]
             },
-            "test": False
+            "name": {
+              "values": ["Modified Test Ad Account"]
+            },
+            "test": True
         },
         "start": 0,
         "count": 5
     },
     access_token=ACCESS_TOKEN,
-    version_string=MDP_VERSION
+    version_string=API_VERSION
 )
 print("Find ad accounts result: ", json.dumps(r.elements))
-print(f"Total results: {r.paging.total}")
+print(f"Total results: {r.paging.total}\n")
 
 
-
-response = restli_client.request(request)
+"""
+Delete ad account
+"""
+delete_response = restli_client.delete(
+  resource_path=AD_ACCOUNTS_ENTITY_RESOURCE,
+  path_keys={
+    "id": ad_account_id
+  },
+  access_token=ACCESS_TOKEN,
+  version_string=API_VERSION
+)
+print("Successfully deleted ad account\n")
--- examples/oauth-member-auth-redirect.py
+++ examples/oauth-member-auth-redirect.py
@@ -2,7 +2,18 @@
 This example illustrates a basic example of the oauth authorization code flow.
 
 Pre-requisites:
-. Add CLIENT_ID, CLIENT_SECRET, and OAUTH2_REDIRECT_URL variables to the top-level .env file.
+1. Add CLIENT_ID, CLIENT_SECRET, and OAUTH2_REDIRECT_URL variables to the top-level .env file.
+The OAUTH2_REDIRECT_URL should be set to "http://localhost:3000/oauth".
+2. The associated developer app you are using should have access to r_liteprofile, which can be
+obtained through requesting the self-serve Sign In With LinkedIn API product on the LinkedIn
+Developer Portal.
+3. Set your developer app's OAuth redirect URL to "http://localhost:3000/oauth" from the Developer Portal
+
+Steps:
+1. Run script: `python3 oauth-member-auth-redirect.py`
+2. Navigate to localhost:3000
+3. Login as LinkedIn member and authorize application
+4. View member profile data
 """
 import os,sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
@@ -29,7 +40,7 @@ restli_client = RestliClient()
 def main():
   global access_token
   if access_token == None:
-    return redirect(auth_client.generate_member_auth_url(scopes=["r_liteprofile","rw_ads"]))
+    return redirect(auth_client.generate_member_auth_url(scopes=["r_liteprofile"]))
   else:
     return restli_client.get(
       resource_path="/me",
@@ -47,6 +58,7 @@ def oauth():
   if auth_code:
     token_response = auth_client.exchange_auth_code_for_access_token(auth_code)
     access_token = token_response.access_token
+    print(f"Access token: {access_token}")
     return redirect('/')
 
 
