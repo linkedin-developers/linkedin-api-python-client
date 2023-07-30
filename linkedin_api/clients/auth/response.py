ef0b3fcbf7aa020f4ecc87bcf499b97aee30b8ce
nbro10 <nelson.brochado@webrepublic.ch>
2023-07-30 16:15:58 +0200

    Fix the docstring indentation issue

--- linkedin_api/clients/auth/response.py
+++ linkedin_api/clients/auth/response.py
@@ -23,28 +23,28 @@ class AccessToken3LResponse(BaseAuthResponse):
         )
         self.access_token = access_token
         """
-      The 3-legged access token.
-      """
+        The 3-legged access token.
+        """
 
         self.expires_in = expires_in
         """
-      The TTL for the access token, in seconds.
-      """
+        The TTL for the access token, in seconds.
+        """
 
         self.refresh_token = refresh_token
         """
-      The refresh token value. Only available if refresh tokens are enabled.
-      """
+        The refresh token value. Only available if refresh tokens are enabled.
+        """
 
         self.refresh_token_expires_in = refresh_token_expires_in
         """
-      The TTL for the refresh token, in seconds. Only available if refresh tokens are enabled.
-      """
+        The TTL for the refresh token, in seconds. Only available if refresh tokens are enabled.
+        """
 
         self.scope = scope
         """
-      A comma-separated list of scopes authorized by the member (e.g. "r_liteprofile,r_ads").
-      """
+        A comma-separated list of scopes authorized by the member (e.g. "r_liteprofile,r_ads").
+        """
 
 
 class AccessToken2LResponse(BaseAuthResponse):
@@ -54,13 +54,13 @@ class AccessToken2LResponse(BaseAuthResponse):
         )
         self.access_token = access_token
         """
-      The two-legged access token.
-      """
+        The two-legged access token.
+        """
 
         self.expires_in = expires_in
         """
-      The TTL of the access token, in seconds.
-      """
+        The TTL of the access token, in seconds.
+        """
 
 
 class IntrospectTokenResponse(BaseAuthResponse):
@@ -84,43 +84,43 @@ class IntrospectTokenResponse(BaseAuthResponse):
         )
         self.active = active
         """
-      Boolean flag whether the token is a valid, active token.
-      """
+        Boolean flag whether the token is a valid, active token.
+        """
 
         self.auth_type = auth_type
         """
-      The auth type of the token ("2L", "3L" or "Enterprise_User")
-      """
+        The auth type of the token ("2L", "3L" or "Enterprise_User")
+        """
 
         self.authorized_at = authorized_at
         """
-      Epoch time in seconds, indicating when the token was authorized.
-      """
+        Epoch time in seconds, indicating when the token was authorized.
+        """
 
         self.client_id = client_id
         """
-      Developer application client ID.
-      """
+        Developer application client ID.
+        """
 
         self.created_at = created_at
         """
-      Epoch time in seconds, indicating when this token was originally issued.
-      """
+        Epoch time in seconds, indicating when this token was originally issued.
+        """
 
         self.expires_at = expires_at
         """
-      Epoch time in seconds, indicating when this token will expire.
-      """
+        Epoch time in seconds, indicating when this token will expire.
+        """
 
         self.scope = scope
         """
-      A string containing a comma-separated list of scopes associated with this token. This is only returned for 3-legged member tokens.
-      """
+        A string containing a comma-separated list of scopes associated with this token. This is only returned for 3-legged member tokens.
+        """
 
         self.status = status
         """
-      The token status ("revoked", "expired", or "active")
-      """
+        The token status ("revoked", "expired", or "active")
+        """
 
 
 class RefreshTokenExchangeResponse(BaseAuthResponse):
@@ -140,20 +140,20 @@ class RefreshTokenExchangeResponse(BaseAuthResponse):
         )
         self.access_token = access_token
         """
-    The 3-legged access token.
-    """
+        The 3-legged access token.
+        """
 
         self.expires_in = expires_in
         """
-    The TTL for the access token, in seconds.
-    """
+        The TTL for the access token, in seconds.
+        """
 
         self.refresh_token = refresh_token
         """
-    The refresh token value.
-    """
+        The refresh token value.
+        """
 
         self.refresh_token_expires_in = refresh_token_expires_in
         """
-    The TTL for the refresh token, in seconds.
-    """
+        The TTL for the refresh token, in seconds.
+        """
